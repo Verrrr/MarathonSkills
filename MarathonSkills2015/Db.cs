589a06948ebe3fc11d2fba1c2098808b50afd029
Verrrr <zylegaspi@gmail.com>
2019-07-25 00:12:13 -0500

    Create Db class

--- MarathonSkills2015/Db.cs
+++ MarathonSkills2015/Db.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Data;
 
 namespace MarathonSkills2015
 {
     class Db
     {
+        MySqlConnection conn;
+        public Db()
+        {
+            string connectionString = "server=localhost;user id=root;database=marathonskills2015";
+            conn = new MySqlConnection(connectionString);
+        }
+
+        public void execute(String sql)
+        {
+            MySqlCommand cmd = new MySqlCommand(sql, this.conn);
+            this.conn.Open();
+            cmd.ExecuteNonQuery();
+            this.conn.Close();
+        }
+
+        public DataTable executeQuery(String sql)
+        {
+            DataTable dt = new DataTable();
+            using (MySqlCommand cmd = new MySqlCommand(sql))
+            {
+                cmd.Connection = this.conn; ;
+                this.conn.Open();
+                using (MySqlDataAdapter a = new MySqlDataAdapter(cmd))
+                {
+                    a.Fill(dt);
+                }
+                this.conn.Close();
+            }
+            return dt;
+        }
     }
 }
