8081cb4d6502ca4eb4360f94b0572774d09150b9
Verrrr <zylegaspi@gmail.com>
2019-08-28 05:04:36 -0500

    initial

--- MarathonSkills2015/Page6.xaml.cs
+++ MarathonSkills2015/Page6.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Data;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,10 +24,20 @@ namespace MarathonSkills2015
     /// </summary>
     public sealed partial class Page6 : Page
     {
+        Db db = new Db();
         public Page6()
         {
             this.InitializeComponent();
             PageController.ChangePageTitle("Sponsor a runner");
+            this.loadRunners();
+        }
+
+        private void loadRunners()
+        {
+            var data = db.executeQuery("SELECT * FROM charity");
+            this.cbRunners.ItemsSource =  data;
+            cbRunners.DisplayMemberPath = "CharityId";
+            
         }
     }
 }
